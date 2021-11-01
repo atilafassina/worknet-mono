bd1a6958c4ac3f272674eae0c4a3746603b09362
Atila Fassina <atila@fassina.eu>
2021-11-01 15:03:02 +0100

    chore: cleanup

--- apps/social/next.config.js
+++ apps/social/next.config.js
@@ -1,39 +1,8 @@
-// const securityHeaders = [
-//   {
-//     key: 'X-DNS-Prefetch-Control',
-//     value: 'on',
-//   },
-//   {
-//     key: 'Strict-Transport-Security',
-//     value: 'max-age=63072000; includeSubDomains; preload',
-//   },
-//   {
-//     key: 'X-Frame-Options',
-//     value: 'DENY',
-//   },
-//   {
-//     key: 'X-XSS-Protection',
-//     value: '1; mode=block',
-//   },
-//   {
-//     key: 'X-Content-Type-Options',
-//     value: 'nosniff',
-//   },
-//   {
-//     key: 'Content-Security-Policy',
-//     value: require('./utils/csp'),
-//   },
-// ]
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   outputFileTracing: false,
   basePath: '/get-started',
-  // i18n: {
-  //   locales: ['en'],
-  //   defaultLocale: 'en',
-  // },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
